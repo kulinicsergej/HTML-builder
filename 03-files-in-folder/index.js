8f422de59ecaf6f7d6e01741bfcf4fdf6d998c21
kulinicsergej <kulinicsergej870@gmail.com>
2024-01-25 08:44:37 +0300

    feat: update 03-fil/kb (Thu, Jan 25, 2024 08:44:37 AM)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,27 @@
-// const { error } = require('console');
+
 const fs = require('fs');
 const path = require('path');
 
 let fn = async () => {
   const pathToFile = path.join(__dirname, 'secret-folder');
   fs.readdir(pathToFile, { withFileTypes: true }, (error, files) => {
-    error ? console.log('ты не прав') : files.forEach((file) => {
-      if (file.isFile()){
-        fs.stat(path.join(pathToFile, file.name), (error, stats) => {
-          error ? console.log('это край!') : console.log(`${file.name.split('.').slice(0,1)}-${path.extname(file.name).split('.').slice(1)}-${stats.size}`);
-        });
-      }
-    });
+    if (error) {
+      console.log('Ошибка чтения директории');
+    } else {
+      files.forEach((file) => {
+        if (file.isFile()){
+          fs.stat(path.join(pathToFile, file.name), (error, stats) => {
+            if (error) {
+              console.log('Ошибка получения информации о файле');
+            } else {
+              let sizeInKb = stats.size / 1024;
+              console.log(`${path.basename(file.name, path.extname(file.name))}-${path.extname(file.name).slice(1)}-${sizeInKb.toFixed(2)}kb`);
+            }
+          });
+        }
+      });
+    }
   });
 }
 fn();
-
 
